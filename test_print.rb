5ec493795a601af43460ce398cd5acfe871ab03a
sawanoboly <sawanoboriyu@higanworks.com>
2013-03-25 06:23:55 +0000

    rename plugin filename

--- test_print.rb
+++ test_print.rb
@@ -7,7 +7,7 @@ require 'ohai'
 ohai = Ohai::System.new
 Ohai::Config[:plugin_path] << File.expand_path("../plugins" , __FILE__) 
 
-puts ohai.require_plugin "joyent_smartmachine"
+puts ohai.require_plugin "joyent"
 
 require 'pp'
 pp(ohai.data)
