f11f0a6785b0900485a4d4f4de9a8a0fb25b0625
sawanoboly <sawanoboriyu@higanworks.com>
2013-03-27 11:22:50 +0900

    Support globalzone

--- plugins/joyent.rb
+++ plugins/joyent.rb
@@ -12,18 +12,26 @@ require_plugin "platform"
 if platform == "smartos" then
   joyent Mash.new
   joyent[:sm_uuid] = `/usr/bin/zonename`.chomp
-  joyent[:sm_id] = `/usr/sbin/zoneadm list -p | awk -F: '{ print $1 }'`.chomp
+  joyent[:sm_id] = `/usr/sbin/zoneadm list -p | awk -F: '{ print $1 }'`.chomp unless joyent[:sm_uuid] == "global"
   
-  # retrieve image
-  sm_image = `cat /etc/product | grep "^Image:" | awk -F': ' '{ print $2 }'`.split(" ")
-  joyent[:sm_image_id] = sm_image[0]
-  joyent[:sm_image_ver] = sm_image[1]
-  
-  ## retrieve baseimage
-  sm_baseimage = `cat /etc/product | grep "^Base Image:" | awk -F': ' '{ print $2 }'`.split(" ")
-  joyent[:sm_baseimage_id] = sm_baseimage[0]
-  joyent[:sm_baseimage_ver] = sm_baseimage[1]
-  
-  ## retrieve pkgsrc
-  joyent[:sm_pkgsrc] = `cat /opt/local/etc/pkg_install.conf | awk -F= '{ print $2 }'`.chomp
+  # retrieve image name and pkgsrc
+  if ::File.exists?("/etc/product") then
+    ::File.open("/etc/product") do |file|
+      while line = file.gets
+        case line
+        when /^Image/
+          sm_image = line.split(" ") 
+          joyent[:sm_image_id] = sm_image[1]
+          joyent[:sm_image_ver] = sm_image[2]
+        when /^Base Image/
+          sm_baseimage = line.split(" ")
+          joyent[:sm_baseimage_id] = sm_baseimage[2]
+          joyent[:sm_baseimage_ver] = sm_baseimage[3]
+        end
+      end
+    end
+
+    ## retrieve pkgsrc
+    joyent[:sm_pkgsrc] = `cat /opt/local/etc/pkg_install.conf | awk -F= '{ print $2 }'`.chomp
+  end
 end
